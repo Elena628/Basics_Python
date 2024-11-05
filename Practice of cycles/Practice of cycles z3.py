2c367ca076862bd4d58f09d6cd0abd16d7ca90c6
Elena628 <hetwenty22@gmail.com>
2024-11-05 20:13:22 +0500

    Correction z3

--- Practice of cycles/Practice of cycles z3.py
+++ Practice of cycles/Practice of cycles z3.py
@@ -1,7 +1,10 @@
 num = int(input("Введите последовательность чисел: "))
-max = 1
+max = float("-inf")
 while num != 0:
     if (num > max) and ((num % 2 == 0) or (num % 3 == 0)) and (num % 10 != 8):
         max = num
     num = int(input())
-print("Наибольшее число в последовательности:", max)
+if max == float("-inf"):
+    print("Последовательность пустая или нет чисел удовлетворяющих условию")
+else:
+    print("Наибольшее число в последовательности:", max)
