294ab32b1e3c0be115127444c0fa5b3d68869c7c
Elena628 <hetwenty22@gmail.com>
2024-11-19 19:17:47 +0500

    New version z2

--- Lists/Lists z2.py
+++ Lists/Lists z2.py
@@ -1,7 +1,11 @@
 num = int(input("Введите последовательность чисел: "))
 a = []
+a2 = []
 while num != 0:
     a.append(num)
     num = int(input())
-a2 = list(set(a))
+for i in a:
+    if i not in a2:
+        a2.append(i)
+# a2 = list(set(a))
 print(sorted(a2, reverse=True))
